b37faf2be6c538d2bbe1e575dc9c47a3cbaabd71
Steven Coutts <stevec@couttsnet.com>
2021-04-09 09:08:02 +0100

    Re-enable logging

--- humidity.py
+++ humidity.py
@@ -3,13 +3,13 @@
 import private as priv
 import time
 import logging
-#from systemd.journal import JournalHandler
+from systemd.journal import JournalHandler
 import paho.mqtt.publish as publish
 import Adafruit_DHT
 
-#log = logging.getLogger('mqtt_alarm')
-#log.addHandler(JournalHandler())
-#log.setLevel(logging.INFO)
+log = logging.getLogger('mqtt_alarm')
+log.addHandler(JournalHandler())
+log.setLevel(logging.INFO)
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
@@ -24,8 +24,8 @@ while True:
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
         publish.single(state_topic, temperature, hostname=priv.MQTT_HOST, client_id='enclosure', auth={'username':priv.username, 'password':priv.password})
-        #log.info("Enclosure Temperature -> " + temperature)
+        log.info("Enclosure Temperature -> " + temperature)
         publish.single(state_topic2, humidity, hostname=priv.MQTT_HOST, client_id='enclosure', auth={'username':priv.username, 'password':priv.password})
-        #log.info("Enclosure Humidity -> " + humidity)
+        log.info("Enclosure Humidity -> " + humidity)
     else:
         print("Failed to retrieve data from humidity sensor")
