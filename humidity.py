4907016bc6010b66d66f55e9cf83e0e1529fe432
Steven Coutts <stevec@couttsnet.com>
2021-04-09 09:01:24 +0100

    Add logging

--- humidity.py
+++ humidity.py
@@ -2,6 +2,7 @@
 #
 import private as priv
 import time
+import logging
 import paho.mqtt.publish as publish
 import Adafruit_DHT
 
@@ -18,6 +19,8 @@ while True:
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
         publish.single(state_topic, temperature, hostname=priv.MQTT_HOST, client_id='enclosure', auth={'username':priv.username, 'password':priv.password})
+        log.info("Enclosure Temperature -> " + temperature)
         publish.single(state_topic2, humidity, hostname=priv.MQTT_HOST, client_id='enclosure', auth={'username':priv.username, 'password':priv.password})
+        log.info("Enclosure Humidity -> " + humidity)
     else:
         print("Failed to retrieve data from humidity sensor")
